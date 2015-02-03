8f5ce14cf6c25bf5d8b086f27ce9871ecf5063ba
Hugo Marchadier <hugo.marchadier@smartplace.pro>
2015-02-03 17:56:43 +0100

    working on textview rotation

--- example/src/main/java/com/marchah/example/uicomponent/OrientableTextView_Demo_Activity.java
+++ example/src/main/java/com/marchah/example/uicomponent/OrientableTextView_Demo_Activity.java
@@ -2,18 +2,12 @@ package com.marchah.example.uicomponent;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import com.marchah.uicomponent.SelectableTypeEditText;
-
-import java.util.LinkedHashMap;
 
 /**
  * Created by marcha on 28/01/15.
  */
 public class OrientableTextView_Demo_Activity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- library/src/com/marchah/uicomponent/OrientableTextView.java
+++ library/src/com/marchah/uicomponent/OrientableTextView.java
@@ -3,10 +3,13 @@ package com.marchah.uicomponent;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
 
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
 
 /**
@@ -35,7 +38,16 @@ public class OrientableTextView extends TextView {
 
         width = getMeasuredWidth();
         height = getMeasuredHeight();
-        translate = getWidth();
+
+        /*Animation animation;
+
+        //Initialize the Animation object
+        animation = new RotateAnimation(angle, angle, 0, 0);
+        //"Save" the results of the animation
+        animation.setFillAfter(true);
+        //Set the animation duration to zero, just in case
+        animation.setDuration(0);
+        setAnimation(animation);*/
     }
 
     private void setAttrs(AttributeSet attrs) {
@@ -52,7 +64,6 @@ public class OrientableTextView extends TextView {
 
     private int width;
     private int height;
-    private int translate;
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
@@ -79,24 +90,76 @@ public class OrientableTextView extends TextView {
                 Log.w("Warning", " angle != 0 && != 180 && != -180 && != -90 && != 90 experimental");
                 super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
+       /* int Ax = getMeasuredWidth();
+        int Ay = getMeasuredHeight();
+        int A2x = (int)( (Ax * Math.cos(-angle)) + (Ay * Math.sin(-angle)));
+        int A2y = (int)(-(Ax * Math.sin(-angle)) + (Ay * Math.cos(-angle)));
+
+        int Bx = getMeasuredWidth();
+        int By = 0;
+        int B2x = (int)( (Bx * Math.cos(-angle)) + (By * Math.sin(-angle)));
+        int B2y = (int)(-(Bx * Math.sin(-angle)) + (By * Math.cos(-angle)));*/
+
+        /*
+        ***********************************************
+        *  Rotation gravity center
+        * *********************************************
+
+        int Ax = getMeasuredWidth() / 2;
+        int Ay = getMeasuredHeight() / 2;
+        Log.v("Debug", "angle: " + angle + ", initWidth: " + Ax + ", initHeight: " + Ay);
+        int A2x = (int)( (Ax * Math.cos(-angle)) + (Ay * Math.sin(-angle)));
+        int A2y = (int)(-(Ax * Math.sin(-angle)) + (Ay * Math.cos(-angle)));
+        Log.v("Debug", "rotation width: " + A2x + ", rotation height: " + A2y);
+
+        width = Math.abs(A2x) * 2;
+        height = Math.abs(A2y) * 2;
+
+
+        Log.v("Debug", "width: " + width + ", height: " + height);
+
+        super.onMeasure(width, height);
+        setMeasuredDimension(width, height);
+*/
+                /*
+
+                ***********************************************
+                *  Rotation single point
+                * *********************************************
+
+                int Ax = getMeasuredWidth();
+                int Ay = getMeasuredHeight();
+
+                int A2x = (int)( (Ax * Math.cos(-angle)) + (Ay * Math.sin(-angle)));
+                int A2y = (int)(-(Ax * Math.sin(-angle)) + (Ay * Math.cos(-angle)));
+
+                width = Math.abs(A2x);
+                height = Math.abs(A2y);
+
+                Log.v("Debug", "angle: " + angle + ", initWidth: " + Ax + ", initHeight: " + Ay);
+                Log.v("Debug", "width: " + width + ", height: " + height);
+*/
+                super.onMeasure(width, height);
+                setMeasuredDimension(width, height);
+
                 width = getMeasuredWidth();
                 height = getMeasuredHeight();
-                Log.v("Debug", "width: " + width + ", height: " + height);
+                //Log.v("Debug", "width: " + width + ", height: " + height);
                 double radian = Math.toRadians(angle);
                 double Ay = Math.cos(radian) * width;
 
                 double Bx = Math.cos(radian) * height;
                 double By = Ay + Math.sin(radian) * height;
-
+                //Log.v("Debug", "Bx: " + Bx + ", By: " + By);
                 double Cx = Bx + Math.cos(radian) * width;
                 double Cy = By + Math.sin(radian) * width;
-
-                double newWidth = Cx;
-                double newHeight = Cy;
+                //Log.v("Debug", "Cx: " + Cx + ", Cy: " + Cy);
+                width = (int)Math.abs(Cx);
+                height = (int)Math.abs(Cy);
                 //translate = (int)Ay;
-                super.onMeasure((int) newWidth, (int) newHeight);
-                setMeasuredDimension((int) newWidth, (int) newHeight);
-                Log.v("Debug", "newWidth: " + newWidth + ", newHeight: " + newHeight);
+                super.onMeasure(width, height);
+                setMeasuredDimension(width, height);
+                //Log.v("Debug", "angle: " + angle + ", newWidth: " + width + ", newHeight: " + height);
                 break;
         }
     }
@@ -108,12 +171,19 @@ public class OrientableTextView extends TextView {
         textPaint.drawableState = getDrawableState();
         canvas.save();
 
+        //Log.v("Debug", "angle: " + angle + ", width: " + getWidth());
+
         if (angle == 90)
             canvas.translate(getWidth(), 0);
         else if (angle == -90)
             canvas.translate(0, getHeight());
         else if (Math.abs(angle) == 180)
             canvas.translate(getWidth(), getHeight());
+        else if (angle < 0)
+            canvas.translate(0, getHeight());
+        /*else if (angle < 0)
+            canvas.translate(0, height);*/
+
         canvas.rotate(angle);
 
         canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
