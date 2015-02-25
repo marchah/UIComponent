cdb96a272c310a0e7518e7c02df4c16adeaeab03
Hugo Marchadier <hugo.marchadier@smartplace.pro>
2015-02-25 18:40:28 +0100

    updates

--- library/src/com/marchah/uicomponent/ImagePreview.java
+++ library/src/com/marchah/uicomponent/ImagePreview.java
@@ -1,8 +1,10 @@
 package com.marchah.uicomponent;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,6 +16,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import java.util.MissingResourceException;
+
 /**
  * Created by marcha on 09/02/15.
  */
@@ -23,6 +27,9 @@ public class ImagePreview extends ImageView implements View.OnClickListener {
 
     private float width;
     private float height;
+
+    private Drawable thumb;
+    private Drawable image;
 
     public ImagePreview(Context context) {
         super(context);
@@ -40,16 +47,26 @@ public class ImagePreview extends ImageView implements View.OnClickListener {
         setId(R.id.uic_imagepreview);
         setOnClickListener(this);
         isFullScreen = false;
-        /*if (attrs != null)
-            setAttrs(attrs);*/
-        Log.v("Debug", "height: " + height + ", width: " + width);
 
+        thumb = null;
+        image = null;
+        if (attrs != null)
+            setAttrs(attrs);
+        if (thumb == null || image == null)
+            throw new MissingResourceException("Thumb or/and image missing", getClass().getName(), "");
+        setImageDrawable(thumb);
     }
 
     private void setAttrs(AttributeSet attrs) {
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.uic_stylable);
         for (int i = 0; i < a.getIndexCount(); i++) {
             int attr = a.getIndex(i);
+            if (attr == R.styleable.uic_stylable_thumb) {
+                thumb = a.getDrawable(attr);
+            }
+            else if (attr == R.styleable.uic_stylable_image) {
+                image = a.getDrawable(attr);
+            }
            /* if (attr == R.styleable.uic_stylable_android_layout_height) {
                 hasHeight = true;
                 //try {
@@ -75,8 +92,9 @@ public class ImagePreview extends ImageView implements View.OnClickListener {
     public void onClick(View v) {
         if (getId() == R.id.uic_imagepreview) {
             if (isFullScreen) {
-                v.setLayoutParams(new RelativeLayout.LayoutParams((int)width, (int)height));
+                v.setLayoutParams(new LinearLayout.LayoutParams((int)width, (int)height));
                 isFullScreen = false;
+                setImageDrawable(thumb);
             }
             else {
                 height = getHeight();
@@ -85,13 +103,14 @@ public class ImagePreview extends ImageView implements View.OnClickListener {
                 WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
                 Display display = wm.getDefaultDisplay();
                 if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1)//HONEYCOMB_MR2)
-                    v.setLayoutParams(new RelativeLayout.LayoutParams(display.getWidth(), display.getHeight()));
+                    v.setLayoutParams(new LinearLayout.LayoutParams(display.getWidth(), display.getHeight()));
                 else {
                     Point size = new Point();
                     display.getRealSize(size);
-                    v.setLayoutParams(new RelativeLayout.LayoutParams(size.x, size.y));
+                    v.setLayoutParams(new LinearLayout.LayoutParams(size.x, size.y));
                 }
                 isFullScreen = true;
+                setImageDrawable(image);
             }
         }
     }
